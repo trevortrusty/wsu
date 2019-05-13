129fb57bd8c5de4b6e1741984a6f13150579f986
Trevor Trusty <tsquared21@outlook.com>
2019-05-13 14:30:22 -0700

    Wrote header comment

--- wsu.cpp
+++ wsu.cpp
@@ -1,3 +1,15 @@
+/////////////////////////////////////////////////////////////
+// Title: WSU
+// Author: Trevor N Trusty
+//
+// Prints a Wayne State University logo in ascii symbols to
+// the console
+////////////////////////////////////////////////////////////
+//
+// Note to self: http://packaging.ubuntu.com/html/getting-set-up.html 
+//               http://packaging.ubuntu.com/html/packaging-new-software.html  
+//               to package software in ubuntu
+
 #include <iostream>
 using namespace std;
 int main()
