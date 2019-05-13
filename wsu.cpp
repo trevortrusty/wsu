895014d2d9ee372273e5283f79633fde21de49e5
Trevor N Trusty <tsquared21@outlook.com>
2019-05-13 16:38:38 -0400

    Added source files

---
+++ wsu.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	cout << " ___________                    ___________ " << endl;
+	cout << " .         /                    \\         ." << endl;
+	cout << " \\        /                      \\        /" << endl;
+	cout << "  .      /                        \\      . " << endl;
+	cout << "  \\      \\                        /      /" << endl;
+	cout << "   .      \\      __________      /     ." << endl;
+	cout << "   \\       \\     \\        /     /      /" << endl;
+	cout << "   .              \\      /             ."  << endl;
+	cout << "    \\       \\     /      \\     /      /" << endl;
+	cout << "     .       \\   /        \\   /      ." << endl;
+	cout << "      \\       \\ /          \\ /      /" << endl;
+	cout << "       \\       .     /\\     .      /" << endl;
+	cout << "         .          /  \\         ." << endl;
+	cout << "          \\       /      \\      /" << endl;
+	cout << "            \\    /        \\   /"  << endl;
+	cout << "               ./          \\." << endl;
+}                 
